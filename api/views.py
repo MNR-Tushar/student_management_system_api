b9b01b5a589c3725871c12432f22ef23bb04e695
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-11-19 23:41:23 +0600

    include GenericViewSet in DepartmentViewset

--- api/views.py
+++ api/views.py
@@ -2,10 +2,44 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import *
 from .models import *
-
-class DepartmentViewset(viewsets.ModelViewSet):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
+from rest_framework.response import Response
+class DepartmentViewset(viewsets.GenericViewSet):
+    queryset=Department.objects.all()
+    serializer_class=DepartmentSerializer
+    def list(self, request):
+        departments=self.get_queryset()
+        serializer=self.get_serializer(departments,many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer=self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def update(self, request, pk=None):
+        department=self.get_object()
+        serializer=self.get_serializer(department,data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def partial_update(self, request, pk=None):
+        department=self.get_object()
+        serializer=self.get_serializer(department,data=request.data,partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def destroy(self, request, pk=None):
+        department=self.get_object()
+        department.delete()
+        return Response(status=204)
+    
+    def retrieve(self, request, pk=None):
+        department=self.get_object()
+        serializer=self.get_serializer(department)
+        return Response(serializer.data)
 
 class StudentsViewset(viewsets.ModelViewSet):
     queryset = Students.objects.all()
