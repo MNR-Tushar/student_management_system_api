46e0ec313a2084b357ec81667d6753a964531170
MNR-Tushar <rahman23105101275@diu.edu.bd>
2025-12-10 23:29:17 +0600

    add model attibute

--- users/models.py
+++ users/models.py
@@ -7,6 +7,13 @@ from .managers import CustomUserManager
 
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    first_name = models.CharField(max_length=30, null=True, blank=True)
+    last_name = models.CharField(max_length=30, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+    
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
